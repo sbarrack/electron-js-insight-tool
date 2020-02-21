15b79ff3991d661e184e524393394153db5829aa
sbarrack <stephen@motionstrand.com>
2020-02-21 14:07:24 -0800

    parses the csv

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# insight/dev #
+# README #
 
 This README would normally document whatever steps are necessary to get your application up and running.
 
--- index.js
+++ index.js
@@ -5,10 +5,39 @@ const path = require('path');
 
 const chalk = require('chalk');
 const meow = require('meow')(rFile('help.txt').toString(), JSON.parse(rFile('meow.json')));
+const csvParse = require('csv-parse');
 
 var config = JSON.parse(rFile('config.json'));
 
-// TODO code here
+// _____________________________________________________
+
+const output = [];
+
+if (!meow.flags.input) {
+  console.log('I need a real file, please.');
+  process.exit();
+}
+const stream = fs.createReadStream(meow.flags.input);
+
+stream.pipe(csvParse({
+  cast: true,
+  comment: '#',
+  skip_empty_lines: true,
+  skip_lines_with_error: true,
+  skip_lines_with_empty_values: true
+}, (e, data) => {
+  if (e) {
+    console.error(e);
+    return;
+  }
+  output.push(data);
+}));
+
+stream.on('close', () => {
+  console.log('Done!');
+});
+
+// _____________________________________________________
 
 /**
  * 
