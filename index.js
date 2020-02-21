aa4db79b50cd6292351147b39aff572ea119d28f
sbarrack <stephen@motionstrand.com>
2020-02-21 11:39:06 -0800

    more setup

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const path = require('path');
 const chalk = require('chalk');
 const meow = require('meow')(rFile('help.txt').toString(), JSON.parse(rFile('meow.json')));
 
+var config = JSON.parse(rFile('config.json'));
+
 // TODO code here
 
 /**
  * 
- * @param {string} file The target file path relative to .
+ * @param {string} file Target file path relative to .
  */
 function rFile(file) {
   return fs.readFileSync(path.join(__dirname, file));
