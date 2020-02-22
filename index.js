43e0472c91844283aded88a68d025411c153f723
sbarrack <stephen@motionstrand.com>
2020-02-21 16:23:08 -0800

    mutates csv rows on input

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const chalk = require('chalk');
-const meow = require('meow')(rFile('help.txt').toString(), JSON.parse(rFile('meow.json')));
-const csvParse = require('csv-parse');
+const chalk = require('chalk')
+const meow = require('meow')(rFile('help.txt').toString(), JSON.parse(rFile('meow.json')))
+const csvParse = require('csv-parse')
 
-var config = JSON.parse(rFile('config.json'));
+var config = JSON.parse(rFile('config.json'))
+
+const outpath = 'index.html'
 
 // _____________________________________________________
 
-const output = [];
+const parsed = []
 
 if (!meow.flags.input) {
-  console.log('I need a real file, please.');
-  process.exit();
+  console.log('I need a real file, please.')
+  process.exit()
 }
-const stream = fs.createReadStream(meow.flags.input);
+const input = fs.createReadStream(meow.flags.input)
 
-stream.pipe(csvParse({
+const rowOp = row => {
+  delete row.undefined
+  row.percentNewUsers = row.newUsers / row.newUsers
+  row.avgSessionsPerUser = row.sessions / row.users
+  return row
+}
+
+input.pipe(csvParse({
   cast: true,
+  columns: config.headers,
   comment: '#',
+  from: 2,
+  on_record: rowOp,
   skip_empty_lines: true,
   skip_lines_with_error: true,
   skip_lines_with_empty_values: true
 }, (e, data) => {
   if (e) {
-    console.error(e);
-    return;
+    console.error(e)
+    return
   }
-  output.push(data);
-}));
+  parsed.push(data)
+}))
 
-stream.on('close', () => {
-  console.log('Done!');
-});
+input.on('close', () => {
+  console.log(parsed)
+  // TODO create HTML
+})
 
 // _____________________________________________________
 
@@ -44,5 +57,5 @@ stream.on('close', () => {
  * @param {string} file Target file path relative to .
  */
 function rFile(file) {
-  return fs.readFileSync(path.join(__dirname, file));
+  return fs.readFileSync(path.join(__dirname, file))
 }
