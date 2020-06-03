eec2c827cadbe9ad12f71a6c88bdf1892cf60344
sbarrack <stephen@motionstrand.com>
2020-06-03 15:42:31 -0700

    Revert "test file dialogue"
    This reverts commit 35eb96939df4f239dc6a58ccb75be837c9c4202f.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const debug = require('electron-debug');
 const contextMenu = require('electron-context-menu');
 
 unhandled();
-debug();
+// debug();
 contextMenu();
 
 let mainWindow;
@@ -16,8 +16,8 @@ const createMainWindow = async () => {
   const win = new BrowserWindow({
     title: app.name,
     show: false,
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 450,
     minHeight: 350,
     minWidth: 370
   });
